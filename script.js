ff8e0ec2aaea591349a869cf8ef45ece8218c3b1
adecarvalho <decarvalho.adelino@gmail.com>
2026-08-08 12:29:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+//****************************** */
+// Gauge temperature
+//****************************** */
+const gaugeTemperature = new Gauge('canvas_temperature_id', {
+	title: 'Temp',
+	unit: '°C',
+	min: -5,
+	max: 50,
+	value: 0,
+	colorNeedle: '#c6d7e7',
+	colorText: '#d3e0ed',
+	tickInterval: 5,
+	zones: [
+		{ limit: 10, color: '#4d4dc3' },
+		{ limit: 20, color: '#f1c40f' },
+		{ limit: 30, color: '#e74c3c' },
+		{ limit: Infinity, color: '#c3106f' },
+	],
+});
+
+//************************** */
+const t1 = setInterval(() => {
+	const val = 5 * (Math.random() - 1) + 20;
+	gaugeTemperature.setValue(val);
+}, 2000);
+//********************************* */
+window.addEventListener('beforeunload', (event) => {
+	clearInterval(t1);
+});
+//end
+
