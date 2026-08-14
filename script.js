ab339036f6d52165c1e9ef33c7e2c91a57375b23
adecarvalho <decarvalho.adelino@gmail.com>
2026-08-14 15:00:03 +0200

    Version 2026-08-15

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const gaugeTemperature = new Gauge('canvas_temperature_id', {
 	value: 0,
 	colorNeedle: '#c6d7e7',
 	colorText: '#d3e0ed',
-	backgroundColor:'rgb(51,51,51)',
+	backgroundColor: 'rgb(51,51,51)',
 	tickInterval: 5,
 	zones: [
 		{ limit: 10, color: '#4d4dc3' },
@@ -19,10 +19,38 @@ const gaugeTemperature = new Gauge('canvas_temperature_id', {
 	],
 });
 
+//************************ */
+// Gauge Humidity
+//************************ */
+const gaugeHumidity = new LcdGauge('canvas_humidity_id', {
+	title: 'Hum',
+	unit: '%',
+	min: 0,
+	max: 100,
+	value: 0,
+	tickInterval: 5,
+});
+
+//************************ */
+// Gauge Lum
+//************************ */
+const gaugeLuminosity = new LcdVerticalGauge('canvas_luminosity_id', {
+	title: 'Lum',
+	unit: 'Lux',
+	min: 0,
+	max: 2000,
+	value: 0,
+	tickInterval: 250,
+});
+
 //************************** */
 const t1 = setInterval(() => {
 	const val = 5 * (Math.random() - 1) + 20;
 	gaugeTemperature.setValue(val);
+
+	gaugeHumidity.setValue(val * 2);
+
+	gaugeLuminosity.setValue(val * 100);
 }, 2000);
 //********************************* */
 window.addEventListener('beforeunload', (event) => {
